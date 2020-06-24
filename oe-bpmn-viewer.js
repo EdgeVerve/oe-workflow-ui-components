dad0343825067e4e403a54b32a06183b6202b204
Bhagyalakshmi M <bhagyalakshmi.m@infosys.com>
2020-06-24 13:14:19 +0530

    corrected the listdata

--- oe-bpmn-viewer.js
+++ oe-bpmn-viewer.js
@@ -239,7 +239,7 @@ class oeBpmnViewer extends GestureEventListeners(OECommonMixin(OEAjaxMixin(Polym
       self.$.approval.open();
     });
     self.set('__taskActionListData', [
-      'accept', 'reject'
+      'accepted', 'rejected'
     ]);
   }
   __or(bool1, bool2) {
